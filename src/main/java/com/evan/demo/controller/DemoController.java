975942c2170cd870d2c3c41e9d67ec348d1671f5
Ruben Marin Lopez <ams.rmarin@ext.aragon.es>
2023-05-17 17:42:08 +0200

    cambio crossorigin

--- src/main/java/com/evan/demo/controller/DemoController.java
+++ src/main/java/com/evan/demo/controller/DemoController.java
@@ -5,11 +5,12 @@ import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 /**
  * Created by Evan on 2017-07-03.
  */
-
+@CrossOrigin
 @RestController
 @RequestMapping("/")
 public class DemoController {
